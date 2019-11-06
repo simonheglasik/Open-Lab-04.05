be6b3e11578cd5ca7f73e66ca975bd9a77c17f6d
simonheglasik <simonheglasik@gmail.com>
2019-11-06 17:29:51 +0100

    04.05

--- Open-Lab-04.05/StringTools.cs
+++ Open-Lab-04.05/StringTools.cs
@@ -6,7 +6,15 @@ namespace Open_Lab_04._05
     {
         public string Repeat(string orig, int n)
         {
-            throw new NotImplementedException();
+            string a = "";
+            for (int i = 0; i < orig.Length; i++)
+            {
+                for (int v = 0; v < n; v++)
+                {
+                    a = a + orig[i];
+                }
+            }
+            return a;
         }
     }
 }
